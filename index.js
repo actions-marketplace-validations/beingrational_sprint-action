4a0f41c7a2d8991af76f6d0ab92bc15920b250c9
ajaykn <40024974+ajaykn@users.noreply.github.com>
2021-02-06 00:10:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  // `whom-to-sprint` input defined in action metadata file
+  const nameToSprint = core.getInput('whom-to-sprint');
+  console.log(`Sprint ${nameToSprint}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
